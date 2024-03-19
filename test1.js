3c2ee5d18b22aea63e7233e0abd82e35cd6b7315
Konghen001 <142234408+Konghen001@users.noreply.github.com>
2024-03-19 16:17:34 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -12,8 +12,16 @@ let obj = JSON.parse($response.body);
 if (obj.data && obj.data.lessons) {
   obj.data.lessons.is_lock = "0";
   obj.data.lessons.is_audition = "1";
- 
-
+  $done({
+    body: JSON.stringify(obj)
+  });
+} else if (obj.data) {
+  obj.data.nickname = "helloWorld";
+  obj.data.vip_text = "永久会员";
+  obj.data.vip_endtime = "98799879987";
+  obj.data.vip_button_text = "successful";
+  obj.data.is_vip = "1";
+  obj.data.vip = true;
   $done({
     body: JSON.stringify(obj)
   });
