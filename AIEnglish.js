b15be1fa363d0e01e4abb318120e0a8f193caaf2
Konghen001 <142234408+Konghen001@users.noreply.github.com>
2024-03-20 09:21:28 +0800

    Update AIEnglish.js

--- AIEnglish.js
+++ AIEnglish.js
@@ -23,4 +23,5 @@ body = body.replace(/"is_vip":\s*"\d"/g, '"is_vip":"1"');
 body = body.replace(/"vip_button_text":".*?"/g, '"vip_button_text":"@TGios151"');
 body = body.replace(/"vip_endtime":\s*"\d"/g, '"vip_endtime":"6000"');
 body = body.replace(/"forbid_end_time":\s*"\d"/g, '"forbid_end_time":4800');
+body = body.replace(/"status":\s*"\d"/g, '"status":"1"');
 $done({ body });
