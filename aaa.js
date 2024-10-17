417ea93884f5819c15ebfbc0802857ebe74231c0
Konghen001 <142234408+Konghen001@users.noreply.github.com>
2024-10-17 23:00:26 +0800

    更新 aaa.js

--- aaa.js
+++ aaa.js
@@ -1,4 +1,12 @@
+/*
+[rewrite_local]
+^https:\/\/billing\.chaton\.ai\/v1\/subscriptions url script-response-body https://raw.githubusercontent.com/Konghen001/myself/main/aaa.js
 
+[mitm] 
+
+hostname = billing.chaton.ai
+
+*/
 
 
 $done({
