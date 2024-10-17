d444af9821b08f245312243291bbee01e41311a4
Konghen001 <142234408+Konghen001@users.noreply.github.com>
2024-10-17 22:57:34 +0800

    更新 aaa.js

---
+++ aaa.js
@@ -0,0 +1,16 @@
+
+
+
+$done({
+  body: JSON.stringify({
+    "subscriptions": [
+      {
+        "id": "696969696969696969",
+        "provider": "apple",
+        "status": "active",
+        "periodEnd": 4102444800,
+        "createdAt": 1728922317
+      }
+    ]
+  })
+});
